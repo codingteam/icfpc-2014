f5f71f0d968794254391b8ccc089c7f3ac60b79e
Friedrich von Never <fvnever@gmail.com>
2014-07-26 17:54:50 +0700

    Additional builtins.

--- Builtins.hs
+++ Builtins.hs
@@ -12,23 +12,51 @@ builtins :: M.Map String Builtin
 builtins = M.fromList [("+", add),
                        ("-", sub),
                        ("*", multiply),
-                       ("/", divide) ]
+                       ("/", divide),
+                       ("=", equal),
+                       (">", greater),
+                       (">=", greaterOrEqual),
+                       ("pair", pair),
+                       ("car", car),
+                       ("cdr", cdr)]
 
 isBuiltin :: String -> Bool
 isBuiltin name = M.member name builtins
 
-chain instruction = \args -> do
-                      forM_ [1..(length args) - 1] (\_ -> i instruction)
+unary :: Instruction -> [SyntaxNode] -> Generator ()
+unary instruction [arg] = i instruction
+unary instruction _     = fail $ "Invalid function call with " ++ (show instruction)
+
+binary :: Instruction -> [SyntaxNode] -> Generator ()
+binary instruction [arg1, arg2] = i instruction
+binary instruction _            = fail $ "Invalid function call with " ++ (show instruction)
 
 add :: Builtin
-add = chain ADD
+add = binary ADD
 
 sub :: Builtin
-sub = chain SUB
+sub = binary SUB
 
 multiply :: Builtin
-multiply = chain MUL
+multiply = binary MUL
 
 divide :: Builtin
-divide = chain DIV
+divide = binary DIV
 
+equal :: Builtin
+equal = binary CEQ
+
+greater :: Builtin
+greater = binary CGT
+
+greaterOrEqual :: Builtin
+greaterOrEqual = binary CGTE
+
+pair :: Builtin
+pair = binary CONS
+
+car :: Builtin
+car = unary CAR
+
+cdr :: Builtin
+cdr = unary CDR
