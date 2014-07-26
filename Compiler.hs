4d7a5b787b0dae0d146f1abc9c2884f12f8c169d
Ilya Portnov <portnov@iportnov.ru>
2014-07-26 14:13:37 +0600

    On compiler.

--- Compiler.hs
+++ Compiler.hs
@@ -1,15 +1,64 @@
-module Compiler(compileMain) where
+{-# LANGUAGE GeneralizedNewtypeDeriving, DeriveDataTypeable, TypeSynonymInstances, FlexibleInstances, OverloadedStrings, ExistentialQuantification #-}
+
+module Compiler (compileMain) where
+
+import Control.Monad
+import Control.Monad.State
+import qualified Data.Map as M
 
 import Syntax
 import Generator
 
+data Context = Context {
+        cName :: String
+      , cVariables :: M.Map String Int
+      } deriving (Eq, Show)
+
+emptyContext :: String -> Context
+emptyContext name = Context name M.empty
+
+data CState = CState {
+        csContexts :: [Context]
+      , csFunctions :: M.Map String (Compiler ())
+      } 
+
+emptyCState :: CState
+emptyCState = CState [] M.empty
+
+newtype Compiler a = Compiler {unCompiler :: StateT CState Generator a}
+  deriving (Monad, MonadState CState)
+
+liftG :: Generator a -> Compiler a
+liftG = Compiler . lift
+
+runCompiler :: Compiler a -> Generator a
+runCompiler comp = evalStateT (unCompiler comp) emptyCState
+
+inContext :: String -> Compiler a -> Compiler a
+inContext name run = do
+  modify $ \st -> st {csContexts = emptyContext name : csContexts st}
+  result <- run
+  modify $ \st -> st {csContexts = drop 1 (csContexts st)}
+  return result
+
 compileMain :: SyntaxNode -> Generator ()
-compileMain (Define "main" (args) body) = compileFunctionBody "main" args body
-compileMain x = error $ (show x) ++ " is not a valid main function"
+compileMain node = runCompiler $ compileMain' node
 
-compileFunctionBody :: String -> [String] -> [SyntaxNode] -> Generator ()
-compileFunctionBody name argNames body = mapM_ compileNode body
+compileMain' :: SyntaxNode -> Compiler ()
+compileMain' (Define "main" (args) body) =
+  inContext "MAIN" $
+      compileFunctionBody "main" args body
+compileMain' x = error $ (show x) ++ " is not a valid main function"
 
-compileNode :: SyntaxNode -> Generator ()
+compileFunctionBody :: String -> [String] -> [SyntaxNode] -> Compiler ()
+compileFunctionBody name argNames body =
+  inContext name $
+      mapM_ compileNode body
+
+compileNode :: SyntaxNode -> Compiler ()
 compileNode (Number x) = do
-    load x
+    liftG $ load x
+compileNode (Define name (args) body) =
+  modify $ \st ->
+    st {csFunctions = M.insert name (compileFunctionBody name args body) (csFunctions st)}
+
