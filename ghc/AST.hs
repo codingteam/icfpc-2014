c8689fd6fca4ad677bcc8955b607d0027d84393e
Alexander Batischev <eual.jp@gmail.com>
2014-07-26 23:53:08 +0400

    Get AST into working shape
    At first I strived to make it as type-safe as possible, but that appears
    to be too hard for me. Working compiler without type-safe AST is better
    than non-existent compiler with any possible set of features, so I'm
    giving up on the idea and just carry on.

--- ghc/AST.hs
+++ ghc/AST.hs
@@ -1,37 +1,54 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 module AST (
   AST(..)
+, Var(..)
+, FuncName
+, FuncArgs
+, VarName
 ) where
 
 import Data.Word (Word8)
 
 type VarName = String
+type FuncName = String
+type FuncArgs = [Var]
 
-data Expr = Const Word8
-          | Var VarName
+data Const = C Word8 deriving Show
+data Var = V VarName deriving Show
 
-data AST = E Expr
-         | Declare VarName
-         | Assign Var E
-         | Inc Var
-         | Dec Var
-         | Add Var E
-         | Sub Var E
-         | Mul Var E
-         | Div Var E
-         | And Var E
-         | Or  Var E
-         | Xor Var E
-         | IfLT E E AST (Maybe AST)
-         | IfEQ E E AST (Maybe AST)
-         | IfGTE  E AST (Maybe AST)
-         | Halt
-         | Go E
-         | GetLMPos E Var Var
-         | GetThisGhostIdx Var
-         | GetGStartPos E Var Var
-         | GetGCurPos E Var Var
-         | GetGParams E Var Var
-         | GlanceAt E E VAR
-         | Debug
-         | Ref E
-         | Deref E
+class Show a => Expr a
+instance Expr Const
+instance Expr Var
+
+data AST where
+  Declare :: VarName -> AST
+  Assign :: Expr a => VarName -> a -> AST
+  Inc :: VarName -> AST
+  Dec :: VarName -> AST
+  Add :: Expr a => VarName -> a -> AST
+  Sub :: Expr a => VarName -> a -> AST
+  Mul :: Expr a => VarName -> a -> AST
+  Div :: Expr a => VarName -> a -> AST
+  And :: Expr a => VarName -> a -> AST
+  Or  :: Expr a => VarName -> a -> AST
+  Xor :: Expr a => VarName -> a -> AST
+  IfLT :: Expr a => a -> a -> AST -> (Maybe AST) -> AST
+  IfEQ :: Expr a => a -> a -> AST -> (Maybe AST) -> AST
+  IfGT :: Expr a => a -> a -> AST -> (Maybe AST) -> AST
+  Halt :: AST
+  Go :: Expr a => a -> AST
+  GetLMPos :: Expr a => a -> VarName -> VarName -> AST
+  GetThisGhostIdx :: VarName -> AST
+  GetGStartPos :: Expr a => a -> VarName -> VarName -> AST
+  GetGCurPos :: Expr a => a -> VarName -> VarName -> AST
+  GetGParams :: Expr a => a -> VarName -> VarName -> AST
+  GlanceAt :: Expr a => a -> a -> VarName -> AST
+  Debug :: AST
+  Ref :: Expr a => a -> AST
+  Deref :: Expr a => a -> AST
+  Func :: FuncName -> FuncArgs -> AST -> AST
+  Block :: [AST] -> AST
+
+deriving instance Show AST
