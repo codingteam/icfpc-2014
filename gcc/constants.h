0801bfebdc0636ab83326481b5ea52ca51a1c029
Ilya Portnov <portnov@iportnov.ru>
2014-07-28 01:41:22 +0600

    2120 achieved.

--- gcc/constants.h
+++ gcc/constants.h
@@ -6,6 +6,9 @@
 #define MYSTART 5
 #define GHOSTSTART 6
 
+#define PILLSCORE 10
+#define POWERPILLSCORE 50
+
 #define UP 0
 #define RIGHT 1
 #define DOWN 2
@@ -16,11 +19,11 @@
 #define TRUE 1
 #define FALSE 0
 
-#define GHOST 20
+#define GHOST 20000
 #define EATABLE 1
 #define PASSABLE 0
-#define WALLCODE 10
+#define WALLCODE 10000
 
-#define VISITED_PENALTY 1000
+#define VISITED_PENALTY 100
 #define INFINITY 1000000
 
